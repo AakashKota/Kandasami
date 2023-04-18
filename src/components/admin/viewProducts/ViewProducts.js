0ad7221918cfefd9bc8e48125d74d43ece183d29
AakashKota <kota.aakash15@gmail.com>
2023-04-19 00:02:43 +0530

    kandasami

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import styles from ".//ViewProducts.modules.scss"
 import { toast } from 'react-toastify';
-import { collection, query, where, onSnapshot, orderBy, doc } from "firebase/firestore";
-import { db } from '../../../firebase/config';
-import { Link } from 'react-router-dom';
+import { collection, query,  onSnapshot, orderBy, deleteDoc, doc } from "firebase/firestore";
+import { db, storage } from '../../../firebase/config';
+import { Await, Link } from 'react-router-dom';
 import {FaEdit, FaTrashAlt} from "react-icons/fa";
+import styles from "./ViewProducts.module.scss";
+import Loader from '../../loader/Loader';
+import { deleteObject, ref } from 'firebase/storage';
+import Notiflix from 'notiflix';
 
 
 const ViewProducts = () => {
@@ -29,22 +32,54 @@ const ViewProducts = () => {
       setProducts(allProducts);
       setIsLoading(false);
   });
-  
-
-
     } catch(error){
       setIsLoading(false);
       toast.error(error);
     }
-  }
+  };
+    const confirmDelete=(id,imageURL)=>{
+      Notiflix.Confirm.show(
+        "Delete Product!!!",
+        "You are about to delete this product",
+        "Delete",
+        "Cancel",
+        function okCb() {
+          deleteProduct(id, imageURL);
+        },
+        function cancelCb() {
+          console.log("Delete Canceled");
+        },
+        {
+          width: "320px",
+          borderRadius: "3px",
+          titleColor: "orangered",
+          okButtonBackground: "orangered",
+          cssAnimationStyle: "zoom",
+        }
+      );
+    };
+
+    const deleteProduct=async(id,imageURL)=>{
+      try{
+        await deleteDoc(doc(db, "products", id));
+
+        const storageRef=ref(storage, imageURL);
+        await deleteObject(storageRef);
+        toast.success("Product deleted successfully.");
+      }catch(error){
+        toast.error(error.message);
+      }
+    }
   return (
     <>
+    {isLoading && <Loader/>}
     <div className={styles.table}>
       <h2>All Product</h2>
       {products.length===0 ? (
         <p>No Product found.</p>
       ):(
         <table>
+          <thead>
           <tr>
             <th>s/n</th>
             <th>Image</th>
@@ -53,9 +88,11 @@ const ViewProducts = () => {
             <th>Price</th>
             <th>Actions</th>         
           </tr>
+          </thead>
+          <tbody>
           {products.map((product,index) => {
-            const{id,name,price,imageURL,category}= product;
-            return(
+            const {id,name,price,imageURL,category} = product;
+            return(              
               <tr key={id}>
                 <td>
                   {index +1}
@@ -66,16 +103,18 @@ const ViewProducts = () => {
                 <td>{name}</td>
                 <td>{category}</td>
                 <td>{`Rs${price}`}</td>
-                <td>
+                <td className={styles.icons}>
                   <Link to="../../admin/addProducts">
                     <FaEdit size={20} color='green'/>
                   </Link>
                   &nbsp;&nbsp;
-                  <FaTrashAlt size={20} color='red'/>
+                  <FaTrashAlt size={20} color='red' onClick={() => confirmDelete(id, imageURL)}/>
                 </td>
               </tr>
-            )
+              
+            );
           })}
+          </tbody>
         </table>
       )}
     </div>
